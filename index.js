5b8c47318a2977f076245d305b4d7efeb7b07e2a
KevinArmache <kevinarmache@gmail.com>
2022-07-15 09:28:20 +0100

    fix: resolve bug

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 const puppeteer = require("puppeteer");
-// const devices = puppeteer.devices;
-// const iPad = devices["iPad"];
+const devices = puppeteer.devices;
+const iPad = devices["iPhone 13"];
 
 // Configuration des options chrome
 
@@ -15,7 +15,7 @@ const chromeOptions = {
 const search = `https://www.instagram.com/explore/tags/`;
 
 // Les tags que nous allons chercher
-const tags = ["Naruto"];
+const tags = ["Naruto", "Sasuke"];
 
 (async () => {
   // Les variables environementales
@@ -26,7 +26,7 @@ const tags = ["Naruto"];
   // Initialisation du navigateur
   const browser = await puppeteer.launch(chromeOptions);
   const page = await browser.newPage();
-  // await page.emulate(iPad);
+  await page.emulate(iPad);
   // Navigation vers la page de connexion
   await page.goto(url, { waitUntil: "networkidle2" });
   await page.waitForTimeout(5000);
@@ -49,21 +49,25 @@ const tags = ["Naruto"];
     await page.goto(link, { waitUntil: "networkidle2" });
     await page.waitForTimeout(10000);
 
+    // Sélection des 3 premiers résultats récemment publiés
+
     for (let y = 1; y <= 3; y++) {
       // Sélection des 3 premiers résultats récemment publiés
-      let photos = "article > div:nth-child(3)";
-      let photo =
-        photos + ` > div > div:nth-child(1) > div:nth-child(${y}) > a`;
+      let photos = "article > div:nth-child(3) > div";
+      let photo = photos + ` > div:nth-child(1) > div:nth-child(${y})`;
       await page.waitForTimeout(5000);
 
       // Click sur la premiere photo
       await page.click(photo);
-      await page.waitForTimeout(10000);
+      await page.waitForSelector("div[aria-hidden='true']");
+      await page.waitForTimeout(5000);
 
       // Click sur le bouton "Like"
+      await page.waitForSelector("section > span > button[type='button']");
       let heart = "section > span > button[type='button']";
       let redheart = "svg[aria-label='Je n’aime plus']";
-      let close = "div > div > div:nth-child(2) > div[role='button']  > div";
+      await page.waitForSelector("nav > div > header > div > div");
+      let close = "nav > div > header > div > div";
 
       if ((await page.$(redheart)) !== null) {
         // Si la photo est déjà likée, on ferme la fenêtre
@@ -73,6 +77,8 @@ const tags = ["Naruto"];
         // Sinon, on clique sur le bouton "Like"
         await page.click(heart);
         await page.waitForTimeout(10000);
+        await page.click(close);
+        await page.waitForTimeout(5000);
       }
     }
   }
